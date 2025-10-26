4648594add6b6edf7f34ecaea9edd7fd425d8e76
Rodrigo Gonçalves <rodrigo.goncalves2@ab-inbev.com>
2025-10-26 19:38:58 -0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Mude para '/nome-do-seu-repo/' se o site não aparecer corretamente
+  base: '/rodrigoemariana/',
 })
